013bd61aa6ada24326fc820aae2c284ec17d1bb7
PAAWSA02LC1010 <Duoc@PAAWSA02LC1010.duoc.acad>
2026-03-27 09:30:48 -0300

    Infra Iniciada

---
+++ infra/security_groups.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "web_sg" {
+  name = "spa"
+  #puerto 80 = http
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  #puerto 22 = ssh
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  #darle salida a internet
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
